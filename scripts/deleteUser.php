f1b9c9640f49aa1abd0508dd929b820172a2a09e
foxcreator <foxcreatorg@gmail.com>
2022-06-13 00:32:25 +0300

    [fix DB]

--- scripts/deleteUser.php
+++ scripts/deleteUser.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once '../config/const.php';
-require_once '../config/helper.php';
+require_once '../config/DB.php';
 $db = new DB();
 foreach ($_POST['check'] as $item){
     $db->deleteUser($item);
